1eb5eed796c433be6e9d842062deb730a1371a1d
EMiheeva <90220808+EMiheeva@users.noreply.github.com>
2023-02-12 16:41:02 +0200

    Create 6function.hpp

---
+++ lab_6/code/6function.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+const int SIZE = 100; 
+void Read(int n, int a[][SIZE]);
+void Write(int n, int a[][SIZE]);
+int Summa(int a);
+int Max(int n, int a[][SIZE]);
+int Min(int n, int a[][SIZE]);
